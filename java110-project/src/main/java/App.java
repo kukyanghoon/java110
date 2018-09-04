d09575eb9669d01a141003d11e1c026a0fb84589
unknown <mikekuk2005@naver.com>
2018-09-04 15:22:11 +0900

    클래스활용

--- java110-project/src/main/java/App.java
+++ java110-project/src/main/java/App.java
@@ -1,9 +1,36 @@
 import java.util.Scanner;
 
 public class App {
-    static String[] names = new String[100];
+    // 여러 속성의 값을 관리하기 쉽도록 사용자 정의 데이터 타입을 만들어 사용한다.
+    static class Member{
+        protected String name;
+        protected String email;
+        protected String password;
+        
+        //인스턴스의 메모리를 다루는 연산자   operater=setter/getter=accesor=property=message
+        public String getName() {
+            return name;
+        }
+        public void setName(String name) {
+            this.name = name;
+        }
+        public String getEmail() {
+            return email;
+        }
+        public void setEmail(String email) {
+            this.email = email;
+        }
+        public String getPassword() {
+            return password;
+        }
+        public void setPassword(String password) {
+            this.password = password;
+        }
+    }
+    /*static String[] names = new String[100];
     static String[] emails = new String[100];
-    static String[] passwords = new String[100];
+    static String[] passwords = new String[100];*/
+    static Member[] members = new Member[100];
     static int index = 0;
     static Scanner keyIn = new Scanner(System.in);
     public static void main(String[] args) {
@@ -15,7 +42,7 @@ public class App {
     static void printMembers()
     {
         for(int i=0; i<index; i++) {
-            System.out.printf("%s, %s, %s\n", names[i], emails[i], passwords[i]);
+            System.out.printf("%s, %s, %s\n", members[i].getName(), members[i].getEmail(), members[i].getPassword());
         }
 
     }
@@ -23,17 +50,19 @@ public class App {
     static void inputMembers()
     {
         while(true) {
-
+            Member m =new Member();
+            
             System.out.println("이름?");
-            names[index] = keyIn.nextLine();
+            m.setName(keyIn.nextLine());
 
             System.out.println("이메일?");
-            emails[index] = keyIn.nextLine();
+            m.setEmail(keyIn.nextLine());
 
             System.out.println("비밀번호?");
-            passwords[index] = keyIn.nextLine();
-
-            index++;
+            m.setPassword(keyIn.nextLine());
+            
+            members[index++]=m;
+           
 
             System.out.println("계속입력?(Y/n)");
             String yn=keyIn.nextLine();
