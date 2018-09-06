e15de5a8dcd455b7072feba30a658ea5d7d69807
unknown <mikekuk2005@naver.com>
2018-09-06 14:42:11 +0900

    LinkedList 생성

--- java110-project/src/main/java/bitcamp/java110/cms/util/LinkedList.java
+++ java110-project/src/main/java/bitcamp/java110/cms/util/LinkedList.java
@@ -36,11 +36,46 @@ public class LinkedList<T> {
     
     public T remove(int index)
     {
-        return null;
+        if(index<0 || index>=length)
+            return null;
+        length--;
+        Node<T> cursor =first;
+        for(int count=0; count<index;count++)
+        {
+            cursor = cursor.next;
+        }
+        
+        if(cursor==first)
+        {
+            first = cursor.next;
+            first.prev=null;
+            return cursor.value;
+        }
+        cursor.prev.next=cursor.next;
+        cursor.next.prev=cursor.prev;
+        return cursor.value;
+        
     }
     
     public void insert(int index, T obj)
     {
+        Node<T> node = new Node<>();
+        node.value =obj;
+        
+        Node<T> cursor =first;
+        for(int count=0; count<index;count++)
+        {
+            cursor = cursor.next;
+        }
+        if(cursor !=first) 
+        {
+            cursor.prev.next=node; // 이전 노드가 새 노드를 가리킨다.
+            node.prev = cursor.prev; // 새 노드는 현재 노드의 이전 노드를 가르킨다.
+        }
+        
+        
+        cursor.prev=node; // 현재 노드는 새 노드를 가리킨다.
+        node.next= cursor;//새 노드의 다음은 현재 노드를 카리킨다.
         
     }
     
